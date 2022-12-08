4cacaa3848b816b107988e29f63d7e36603705df
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-12-08 18:49:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/UNKNOWN8884/massilama.git /massilama
-cd /massilama
-echo "Installing Requirements..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/UNKNOWN8884/massilama7/.git /massilama7
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /massilama7
+fi
+cd /massilama7
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 [Lily]..."
 python3 bot.py
